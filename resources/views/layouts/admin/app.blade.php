0f85676f3aac330db6778ff077ec12407d9d446f
Priyank2310 <priyankmaheshwari23@gmail.com>
2023-12-27 14:40:39 +0530

    Add seeders and seed data for user and role and add category feauture

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -64,6 +64,19 @@
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
                         >Create User</a
                     >
+
+                    <a
+                    href="{{ route('admin-categories') }}"
+                    class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
+                    >All Categories</a
+                    >
+
+                    <a
+                    href="{{ route('admin-categories-create') }}"
+                    class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
+                    >Create Category</a
+                    >
+
                     <a
                         href="#"
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
